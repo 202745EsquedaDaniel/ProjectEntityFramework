76ab752c0e6348527731570e8893b60c1f7175c6
eduardani54 <eduardani54@gmail.com>
2025-01-06 18:38:28 +0000

    Entity Framework Config

---
+++ TareasContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using projectentityframework.Models;
+
+namespace projectentityframework;
+
+public class TareasContext: DbContext
+{
+    public DbSet<Categoria> Categorias {get;set;}
+    public DbSet<Tarea> Tareas {get;set;}
+    public TareasContext(DbContextOptions<TareasContext> options) :base(options) {}
+}
